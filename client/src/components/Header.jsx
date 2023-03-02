8f227c447df2126cd8aff5c98a82100d0349671b
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-03-03 02:17:30 +0530

    logout.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useContext } from 'react';
 
 import logo from '../img/60995.png'
 import user from '../img/User image.png'
 const Header = () => {
-  
+  const navigate = useNavigate();
   var modal = document.getElementById("myModal");
   const Rot = document.getElementById("Btn");
 
- 
+
   // useEffect(
   //   () => {
   //     fetch('http://localhost:5000/api/auth/profile',
@@ -18,7 +18,7 @@ const Header = () => {
   //         credentials: 'include',
   //       }).then(response => {
   //         response.json().then(userInfo => {
-         
+
   //         });
 
   //       });
@@ -27,9 +27,9 @@ const Header = () => {
   //   []
 
   // )
- 
 
- 
+
+
   const fun1 = () => {
     const dis = modal.style.display;
 
@@ -51,36 +51,38 @@ const Header = () => {
       modal.style.display = "none";
     }
   }
+  const handlelogout=()=>{
+    localStorage.removeItem('token');
+    navigate('/login');
+  }
   return (
     <div>
       <header>
         <Link to='/' className='logo'>My Blog</Link>
         <nav>
-          
-           
-              <Link to="/create" className='hover-box'>Create New Post</Link>
-              {/* eslint-disable-next-line */}
-              <a >Logout</a>
+
+          {
+          !localStorage.getItem('token')? <><Link to="/create" className='hover-box'>Create New Post</Link>
+           {/* eslint-disable-next-line */}
+          <a onclick={handlelogout}>Logout</a>
           <div className="user-img pointer" ><img src={user} alt="" /></div>
           <div className="myBtn" onClick={fun1}  >
             <img id='Btn' onClick={Rotate} alt="" src={logo} />
-          </div>
-        
-         
-              <Link to="/login" className='Button'>Login</Link>
-              <Link to="/signup" className='Button'>Register</Link>
+          </div> </>: <><Link to="/login" className='Button'>Login</Link>
+          <Link to="/signup" className='Button'>Register</Link></>
+          }
        
-        
+           
         </nav>
       </header>
       <div id="myModal" className="modal">
-        <div className="modal-content"> 
-        <div className="user pointer hover-box"></div>
-        <Link to="/profile" className="edit-profile pointer hover-box">Profile</Link>
-        <div className='Button pointer' >Logout</div>
+        <div className="modal-content">
+          <div className="user pointer hover-box"></div>
+          <Link to="/profile" className="edit-profile pointer hover-box">Profile</Link>
+          <div className='Button pointer' >Logout</div>
         </div>
       </div>
-      
+
     </div>
   )
 }
