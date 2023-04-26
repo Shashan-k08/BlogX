a2de212bbbbf56e62b703713fee749e26c183e10
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-04-26 15:17:37 +0530

    modal updated.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,37 +1,32 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 import logo from '../img/60995.png'
 import user from '../img/User image.png'
 
 const Header = () => {
   const navigate = useNavigate();
-  const fun1 = () => {
-    const modal = document.getElementById("myModal");
-    const dis = modal.style.display;
-    const Rot = document.getElementById("Btn");
-    Rot.style.transform = "rotate(180deg)";
-    if (dis === "block" || dis === "") {
-      modal.style.display = "none";
+  const [modaldisplay, setmodaldisplay] = useState(false)
+  // const fun1 = () => {
+  //   const modal = document.getElementById("myModal");
+  //   const dis = modal.style.display;
 
-    }
+  //   const Rot = document.getElementById("Btn");
+  //   Rot.style.transform = "rotate(180deg)";
+  //   if (dis === "block" || dis === "") {
+  //     modal.style.display = "none";
 
-    if (dis === "none" || dis === "") {
-      modal.style.display = "block";
-    }
-  }
+  //   }
+
+  //   if (dis === "none" || dis === "") {
+  //     modal.style.display = "block";
+  //   }
+  // }
   // const Rotate = () => {
   //   const Rot = document.getElementById("Btn");
   //   Rot.style.transform = "rotate(180deg)";
   // }
 
-  window.onclick = function (event) {
-    // const modal = document.getElementById("myModal")
-    if (event.target.id !== "Btn") {
-      console.log("heylll")
-      document.getElementById("myModal").style.display = "none";
-    }
-  }
 
   const handlelogout = () => {
     localStorage.removeItem('token');
@@ -45,23 +40,23 @@ const Header = () => {
 
           {localStorage.getItem('token') ? <><Link to="/create" className='hover-box'>Create New Post</Link>
             <div className="user-img pointer" ><img src={user} alt="" /></div>
-            <div className="myBtn" id='myBtn' onClick={fun1}  >
-              <img id='Btn'  alt="" src={logo} />
+            <div className="myBtn" id='myBtn' onClick={() => setmodaldisplay(!modaldisplay)} >
+              <img id='Btn' alt="" src={logo} />
             </div> </> : <> <Link to="/login" className='Button'>Login</Link>
             <Link to="/signup" className='Button'>Register</Link> </>
           }
 
 
         </div>
-        <div id="myModal" className="modal">
-        <div className="modal-content">
-          <div className="user pointer hover-box"></div>
-          <Link to="/profile" className=" Button  pointer">Profile</Link>
-          <div className='Button pointer' onClick={handlelogout}>Logout</div>
-        </div>
-      </div>
+        {modaldisplay && <div id="myModal" className="modal">
+          <div className="modal-content">
+            <div className="user pointer hover-box"></div>
+            <Link to="/profile" className=" Button  pointer">Profile</Link>
+            <div className='Button pointer' onClick={handlelogout}>Logout</div>
+          </div>
+        </div>}
       </header>
-      
+
 
     </div>
   )
